9ee8317078c0fe4eb11d7174300765ccfd5f384d
Indre-V <vilickaite@yahoo.ie>
2024-09-10 16:13:20 +0100

    add: navigation.html

--- teacup_tales_app/settings.py
+++ teacup_tales_app/settings.py
@@ -147,7 +147,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+# dir to upload user generated content
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+# dir to static files in production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
