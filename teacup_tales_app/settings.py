30b3c6e7e45e1241d8297db7df1513df7a1f649f
Indre-V <vilickaite@yahoo.ie>
2024-09-17 09:33:06 +0100

    update: database settings

--- teacup_tales_app/settings.py
+++ teacup_tales_app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 from django.contrib import messages
 if os.path.isfile('env.py'):
     import env  # pylint: disable=unused-import
@@ -131,12 +132,17 @@ WSGI_APPLICATION = 'teacup_tales_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
