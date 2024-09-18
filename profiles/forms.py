90b09d646bdf3119242b9758d74792776e7163ca
Indre-V <vilickaite@yahoo.ie>
2024-09-18 22:30:07 +0100

    update: user profiles views

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,35 +1,42 @@
 """Profile forms imports"""
+# forms.py
 from django import forms
+from django.core.exceptions import ValidationError
+import re
 from .models import UserProfile
-
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        exclude = ('user',)
-
-    def __init__(self, *args, **kwargs):
-        """
-        Add placeholders and classes, remove auto-generated
-        labels and set autofocus on first field
-        """
-        super().__init__(*args, **kwargs)
-        placeholders = {
-            'default_phone_number': 'Phone Number',
-            'default_postcode': 'Postal Code',
-            'default_town_or_city': 'Town or City',
-            'default_street_address1': 'Street Address 1',
-            'default_street_address2': 'Street Address 2',
-            'default_county': 'County, State or Locality',
+        fields = [
+            'phone_number', 'default_street_address1', 'default_street_address2', 
+            'default_town_or_city', 'default_county', 'default_postcode', 
+            'default_country'
+        ]
+        widgets = {
+            'default_country': forms.Select(attrs={'class': 'form-select'}),
+            'phone_number': forms.TextInput(attrs={'class': 'form-control'}),
+            # Other widgets as needed
         }
 
-        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
-        for field in self.fields:
-            if field != 'default_country':
-                if self.fields[field].required:
-                    placeholder = f'{placeholders[field]} *'
-                else:
-                    placeholder = placeholders[field]
-                self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'  # noqa
-            self.fields[field].label = False
+    def clean_default_country(self):
+        country = self.cleaned_data.get('default_country')
+        if not country:
+            raise ValidationError("Please select a country.")
+        return country
+
+    def clean_default_town_or_city(self):
+        city = self.cleaned_data.get('default_town_or_city')
+        if city is None:
+            raise ValidationError("City cannot be empty.")
+        if re.search('[^a-zA-Z]', city):
+            raise ValidationError("City name should only contain alphabets.")
+        return city.capitalize()
+
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data.get('phone_number')
+        if phone_number is None:
+            raise ValidationError("Phone number cannot be empty.")
+        if re.search('[^0-9]', phone_number):
+            raise ValidationError("Phone number should only contain numbers.")
+        return phone_number
--- profiles/views.py
+++ profiles/views.py
@@ -1,17 +1,29 @@
-# views.py
-from django.shortcuts import render, get_object_or_404
+"""Profiles views imports"""
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .models import UserProfile
 from .forms import UserProfileForm
-
+from .models import UserProfile
 
 @login_required
 def view_profile(request):
-    """View to display the user's profile"""
+    """
+    Profile view to display and update user profile.
+    """
     user_profile = get_object_or_404(UserProfile, user=request.user)
-    
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=user_profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Thanks {user_profile.user.get_full_name()}, your details have been updated.')
+            return redirect('view_profile')  # Redirect to avoid form resubmission
+    else:
+        form = UserProfileForm(instance=user_profile)
+
     context = {
-        'user_profile': user_profile
+        'form': form,
+        'profile': user_profile,  
     }
+
     return render(request, 'profiles/profile.html', context)
