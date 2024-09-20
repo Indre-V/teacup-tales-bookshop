0fc2bb41b5c51f9de1fba1ecb926b981c3fd2829
Indre-V <vilickaite@yahoo.ie>
2024-09-20 16:46:14 +0100

    add: review app

--- teacup_tales_app/settings.py
+++ teacup_tales_app/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'stock_admin',
     'cart',
     'profiles',
+    'reviews',
 ]
 
 MIDDLEWARE = [
--- teacup_tales_app/urls.py
+++ teacup_tales_app/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('management/', include ('stock_admin.urls')),
     path('cart/', include ('cart.urls')),
     path('profile/', include ('profiles.urls')),
+    path('reviews/', include ('reviews.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
