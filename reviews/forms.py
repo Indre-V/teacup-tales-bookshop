67159e747972006735881c853f5ae0bf5b0ae419
Indre-V <vilickaite@yahoo.ie>
2024-09-20 17:17:05 +0100

    add: review model

---
+++ reviews/forms.py
@@ -0,0 +1,25 @@
+"""Review Form imports"""
+from django import forms
+
+from .models import Review
+
+
+class ReviewProductForm(forms.ModelForm):
+    """
+    A form for creating and updating roduct reviews.
+    """
+    content = forms.CharField(
+        max_length=500,
+        widget=forms.Textarea(attrs={
+            'rows': 5,
+            'cols': 30
+        })
+    )
+
+    class Meta:
+        """
+        Meta options for the CommentForm.
+        Specifies the model to use and the fields to include in the form.
+        """
+        model = Review
+        fields = ['rating', 'content']
--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,47 @@
+"""Review models imports"""
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+from products.models import Product
 
-# Create your models here.
+# pylint: disable=locally-disabled, no-member
+
+RATING = (
+    (1,  "★☆☆☆☆"),
+    (2,  "★★☆☆☆"),
+    (3,  "★★★☆☆"),
+    (4,  "★★★★☆"),
+    (5,  "★★★★★"),
+)
+
+class Review(models.Model):
+    """
+    Model representing product reviews by users
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    content = models.TextField(max_length=300, blank=True)
+    rating = models.IntegerField(choices=RATING, default=None)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def clean(self):
+        super().clean()
+        if self.rating is not None:
+            validators = [
+                MinValueValidator(1, message="Rating must be at least 1."),
+                MaxValueValidator(5, message="Rating must be at most 5."),
+            ]
+            for validator in validators:
+                validator(self.rating)
+
+    class Meta:
+        """
+        Sets the order of comments by date ascending
+        """
+
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.content} by {self.user.username}"
+	
