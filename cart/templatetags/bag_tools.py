ea7dfbad790b48884d34f4718a5604e659659efb
Indre-V <vilickaite@yahoo.ie>
2024-09-21 22:42:10 +0100

    fix: quanity display

--- cart/templatetags/bag_tools.py
+++ cart/templatetags/bag_tools.py
@@ -6,3 +6,7 @@ register = template.Library()
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
     return price * quantity
+
+@register.inclusion_tag('components/quantity-selector.html')
+def quantity_selector(product, quantity):
+    return {'product': product, 'quantity': quantity}
