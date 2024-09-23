49460bd87c7ae805eb704024918ee1ba6cd7e8c5
Indre-V <vilickaite@yahoo.ie>
2024-09-23 21:36:09 +0100

    add: checkout model

---
+++ checkout/models.py
@@ -0,0 +1,119 @@
+"""Checkout model imports"""
+import uuid
+
+from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+from django_countries.fields import CountryField
+
+from products.models import Product
+from profiles.models import UserProfile
+from coupons.models import Coupon
+
+# pylint: disable=locally-disabled, no-member
+
+STATUS = (
+    ('pending', 'Pending'),
+    ('processed', 'Processed'),
+    ('shipped', 'Shipped'),
+    ('cancelled', 'Cancelled'),
+)
+
+class Order(models.Model):
+    """
+    Represents an order made by a user.
+    """
+    order_number = models.CharField(max_length=32, null=False, editable=False,  unique=True)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    coupon = models.ForeignKey(Coupon, null=True, blank=True, on_delete=models.SET_NULL)
+    discount = models.DecimalField(
+        blank=True, null=True, max_digits=6, decimal_places=2)
+    
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+    status = models.CharField(
+        max_length=200, null=True, blank=True, choices=STATUS,
+        default='pending')
+
+
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex[:12].upper()
+
+    def get_total(self):
+        """
+        Update grand total each time a line item is added,
+        accounting for delivery costs and discounts.
+        """
+        self.order_total = self.lineitems.aggregate(Sum
+                                                    ('lineitem_total'))['lineitem_total__sum'] or 0
+        self.discount = 0
+
+        if self.coupon:
+            if self.coupon.discount_type == 'percentage':
+                self.discount = (self.coupon.discount_value / 100) * self.order_total
+            elif self.coupon.discount_type == 'amount':
+                self.discount = self.coupon.discount_value
+        self.discount = min(self.discount, self.order_total)
+
+        discounted_total = self.order_total - self.discount
+
+        if discounted_total < settings.DELIVERY_THRESHOLD:
+            self.delivery_cost = settings.DELIVERY_FEE
+        else:
+            self.delivery_cost = 0
+
+        self.grand_total = discounted_total + self.delivery_cost
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+
+class OrderLineItem(models.Model):
+    """
+    Represents an item in an order.
+    """
+    order = models.ForeignKey(Order, null=False, blank=False,
+                              on_delete=models.CASCADE,
+                              related_name='lineitems')
+    product = models.ForeignKey(
+        Product, null=False, blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0, null=False, blank=False)
+    lineitem_total = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False, blank=False,
+        editable=False)
+
+    def save(self, *args, **kwargs):
+        """ Set the line item total """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'ISBN{self.product.isbn} on order {self.order.order_number}'
--- teacup_tales_app/settings.py
+++ teacup_tales_app/settings.py
@@ -61,7 +61,8 @@ INSTALLED_APPS = [
     'cart',
     'profiles',
     'reviews',
-    'coupons.apps.CouponsConfig',
+    'coupons',
+    'checkout',
 ]
 
 MIDDLEWARE = [
