060dd11912cce2b0d4ec83019198415c264a8a65
Indre-V <vilickaite@yahoo.ie>
2024-09-24 16:16:09 +0100

    add: checkout success

---
+++ checkout/__init__.py
@@ -0,0 +1,5 @@
+"""
+Set the default app configuration for custom app 'checkout'
+to 'checkout.apps.CheckoutConfig' for custom app-specific settings.
+"""
+default_app_config = 'checkout.apps.CheckoutConfig'
--- checkout/views.py
+++ checkout/views.py
@@ -1,16 +1,17 @@
 """Checkout View Imports"""
 import json
 import stripe
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
 from django.contrib import messages
 from django.conf import settings
 from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
 from products.models import Product
 from cart.contexts import cart_contents
 from .forms import CheckoutForm
-from .models import OrderLineItem
+from .models import OrderLineItem, Order
 
 
 
@@ -77,7 +78,7 @@ def checkout(request):
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
     else:
-        cart = request.session.get('bag', {})
+        cart = request.session.get('cart', {})
         if not cart:
             messages.error(request, "There's nothing in your bag at the moment")
             return redirect(reverse('all-products'))
@@ -90,7 +91,7 @@ def checkout(request):
             amount=stripe_total,
             currency=settings.STRIPE_CURRENCY,
         )
-
+        print(intent)
         # Attempt to prefill the form with any info the user maintains in their profile
         if request.user.is_authenticated:
             try:
@@ -98,7 +99,7 @@ def checkout(request):
                 order_form = CheckoutForm(initial={
                     'full_name': profile.user.get_full_name(),
                     'email': profile.user.email,
-                    'phone_number': profile.default_phone_number,
+                    'phone_number': profile.phone_number,
                     'country': profile.default_country,
                     'postcode': profile.default_postcode,
                     'town_or_city': profile.default_town_or_city,
@@ -120,6 +121,49 @@ def checkout(request):
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
+    }
+
+    return render(request, template, context)
+
+
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
+
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    if 'bag' in request.session:
+        del request.session['cart']
+
+    template = 'checkout/checkout-success.html'
+    context = {
+        'order': order,
     }
 
     return render(request, template, context)
