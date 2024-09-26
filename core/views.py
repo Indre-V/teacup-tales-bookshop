3d368311e13965bd0af953beda8309c2e11c9077
Indre-V <vilickaite@yahoo.ie>
2024-09-26 23:11:57 +0100

    refactor: product card

--- core/views.py
+++ core/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from products.filters import ProductFilter
 from products.models import Product
 
+# pylint: disable=locally-disabled, no-member
+
 def index(request):
     """ A view to return the index page """
     filter = ProductFilter(request.GET or None, queryset=Product.objects.none())
