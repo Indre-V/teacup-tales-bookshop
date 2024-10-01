df8e395256dfaff6432c62aa98917b355a058052
Indre-V <vilickaite@yahoo.ie>
2024-10-01 21:14:20 +0100

    update: filter label

--- products/forms.py
+++ products/forms.py
@@ -1,11 +1,24 @@
+"""Forms Imports"""
 from django import forms
 
+
 class SortForm(forms.Form):
+    """
+    Form to allow users to sort products on the product listing page.
+
+    """
+
     SORT_CHOICES = [
+        ('', 'Sort by'),
         ('title_asc', 'Title (A to Z)'),
         ('title_desc', 'Title (Z to A)'),
         ('price_asc', 'Price (Low to High)'),
         ('price_desc', 'Price (High to Low)'),
     ]
 
-    sort_by = forms.ChoiceField(choices=SORT_CHOICES, required=False, widget=forms.Select(attrs={'class': 'form-select'}))
+    sort_by = forms.ChoiceField(
+        choices=SORT_CHOICES,
+        required=False,
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
+
