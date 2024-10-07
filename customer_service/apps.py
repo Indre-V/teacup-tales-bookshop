05d6448ea3919439d8894af6eb1b527162671424
Indre-V <vilickaite@yahoo.ie>
2024-10-07 22:01:24 +0100

    refactor: customer service py for testing

--- customer_service/apps.py
+++ customer_service/apps.py
@@ -1,6 +1,10 @@
+"""Apps imports"""
 from django.apps import AppConfig
 
 
 class CustomerServiceConfig(AppConfig):
+    """
+    Configuration class for the 'customer_service' app.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'customer_service'
